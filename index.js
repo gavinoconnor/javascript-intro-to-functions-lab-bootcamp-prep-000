851f6ca68fa30df3c8908ae9d70f0ee65936e1f1
Gavin O'Connor <gedmund@gmail.com>
2017-09-27 01:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var uppercase = 'HELLO'
-  if (string === uppercase) {
-    return "YES INDEED!"
+  var lowercase = 'hello'
+  if (string === lowercase) {
+    return "I can't hear you!"
   }
 }
 
