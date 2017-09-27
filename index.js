ff8ddb6f9da1c644226a302034d6d229c23cb29b
Gavin O'Connor <gedmund@gmail.com>
2017-09-27 02:42:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   var lowercase = whisper(string)
   var uppercase = shout(string)
-  if (string = lowercase) {
+  if (string = uppercase) {
     return "I can't hear you!"
-  } else if (string = uppercase) {
+  } else if (string = lowercase) {
       return "YES INDEED!"
     } else if (string = "I love you, Grandma.") {
       return "I love you, too."
